02f2c8908a7e7d358d4d5e3adf412e16c5bcf94a
JD17VM <jdiegodiego4@gmail.com>
2024-12-11 10:11:57 -0500

    feat(proceso): añadir proceso 2 webs

--- projects/IHC/src/proceso.jsx
+++ projects/IHC/src/proceso.jsx
@@ -9,7 +9,48 @@ import './assets/styles/estilos_proceso.css'
 
 import imageHelper from './utils/imageHelper'
 
-/*const currentPath = window.location.pathname;
+const pageData = [
+  { 
+      lista_etapas: [
+        {
+          nombre : "Creacion de la idea",
+          texto : "Surge la necesidad de enseñar física de forma interactiva y divertida. Se concibe un juego de aventura con niveles que aplican conceptos como movimiento parabólico, MRUV y caída libre. Surge la necesidad de enseñar física de forma interactiva y divertida. Se concibe un juego de aventura.",
+          imagen: imageHelper.imagen_LL_proceso_1
+        },
+        {
+          nombre : "Sketching",
+          texto : "Decidimos realizar un ejercicio de sketching para nuestro proyecto con el objetivo de recopilar, explorar y analizar diversas ideas de forma colaborativa. Este enfoque nos ayudó a visualizar conceptos que, de otra manera, podrían haberse perdido en discusiones abstractas o notas textuales.",
+          imagen: imageHelper.imagen_LL_proceso_2
+        },
+        {
+          nombre : "Storyboarding",
+          texto : "A través del storyboarding, ilustramos paso a paso la historia del usuario: desde el momento en que identifica una necesidad hasta cómo intenta interactuar con el sistema para resolverla. En cada viñeta, destacamos los momentos críticos, como la confusión al navegar por una interfaz compleja, el tiempo perdido buscando una función específica o la frustración ante respuestas poco claras.",
+          imagen: imageHelper.imagen_LL_proceso_3
+        },
+        {
+          nombre : "Primera Fase",
+          texto : "En esta fase inicial, los usuarios ingresan a un escenario cuidadosamente diseñado que simula un laboratorio interactivo o un entorno natural, dependiendo del tema de física seleccionado. Aquí, tienen la oportunidad de explorar libremente, manipular objetos virtuales y observar cómo se comportan en relación con leyes básicas, como la gravedad, el movimiento o las fuerzas.",
+          imagen: ""
+        },
+      ]
+  },
+  { 
+    lista_etapas: [
+      {
+        nombre : "Creacion de la idea",
+        texto : "En esta fase inicial, los usuarios ingresan a un escenario cuidadosamente diseñado que simula un laboratorio interactivo o un entorno natural, dependiendo del tema de física seleccionado. Aquí, tienen la oportunidad de explorar libremente, manipular objetos virtuales y observar cómo se comportan en relación con leyes básicas, como la gravedad, el movimiento o las fuerzas.",
+        imagen: ""
+      },
+      {
+        nombre : "Creacion de la idea",
+        texto : "En esta fase inicial, los usuarios ingresan a un escenario cuidadosamente diseñado que simula un laboratorio interactivo o un entorno natural, dependiendo del tema de física seleccionado. Aquí, tienen la oportunidad de explorar libremente, manipular objetos virtuales y observar cómo se comportan en relación con leyes básicas, como la gravedad, el movimiento o las fuerzas.",
+        imagen: ""
+      },
+    ]
+  }
+];
+
+const currentPath = window.location.pathname;
 
 let pageDataToUse;
 if (currentPath === '/proceso.html') {
@@ -19,22 +60,19 @@ if (currentPath === '/proceso.html') {
 } else {
   // Manejar rutas no encontradas
   pageDataToUse = { titulo: "Error", contenido: <p>Página no encontrada</p> };
-}*/
+}
 
 createRoot(document.getElementById('root')).render(
   <>
     <Navegador></Navegador>
 
     <div className="cont_proceso">
-        <h1>Proceso</h1>
-
-        <Etapa contenido="En esta fase inicial, los usuarios ingresan a un escenario cuidadosamente diseñado que simula un laboratorio interactivo o un entorno natural, dependiendo del tema de física seleccionado. Aquí, tienen la oportunidad de explorar libremente, manipular objetos virtuales y observar cómo se comportan en relación con leyes básicas, como la gravedad, el movimiento o las fuerzas."
-        enlace_imagen = {imageHelper.imagen_AA_proceso_1}
-        >Creación de la Idea</Etapa>  
-
-        <Etapa contenido="En esta fase inicial, los usuarios ingresan a un escenario cuidadosamente diseñado que simula un laboratorio interactivo o un entorno natural, dependiendo del tema de física seleccionado. Aquí, tienen la oportunidad de explorar libremente, manipular objetos virtuales y observar cómo se comportan en relación con leyes básicas, como la gravedad, el movimiento o las fuerzas."
-        enlace_imagen = {imageHelper.imagen_AA_proceso_2}
-        >Otro</Etapa> 
+      <h1>Proceso</h1>
+      {
+        pageDataToUse.lista_etapas.map((etapa, index) => ( 
+          <Etapa key={index} contenido={etapa.texto} enlace_imagen={etapa.imagen} >{etapa.nombre}</Etapa>  
+        ))
+      }
         
     </div>
 
--- projects/IHC/src/utils/imageHelper.jsx
+++ projects/IHC/src/utils/imageHelper.jsx
@@ -1,6 +1,10 @@
-import imagen_AA_proceso_1 from '../assets/imgs/Arrow_Ascend/Proceso/img1.png' 
-import imagen_AA_proceso_2 from '../assets/imgs/Arrow_Ascend/Proceso/img2.jpg' 
-import imagen_AA_proceso_3 from '../assets/imgs/Arrow_Ascend/Proceso/img3.jpg'
+import imagen_AA_proceso_1 from '../assets/imgs/Arrow_Ascend/Proceso/img1.png';
+import imagen_AA_proceso_2 from '../assets/imgs/Arrow_Ascend/Proceso/img2.jpg';
+import imagen_AA_proceso_3 from '../assets/imgs/Arrow_Ascend/Proceso/img3.jpg';
+
+import imagen_LL_proceso_1 from '../assets/imgs/Launch_VR/Proceso/img1.png';
+import imagen_LL_proceso_2 from '../assets/imgs/Launch_VR/Proceso/img2.jpg';
+import imagen_LL_proceso_3 from '../assets/imgs/Launch_VR/Proceso/img3.jpg';
 
 import logo_IHC_normal from '../assets/imgs/logos/Logo_IHC_Normal.png';
 import logo_LaunchLab_blanco from '../assets/imgs/logos/Launch_Lab_Blanco.png';
@@ -17,6 +21,10 @@ export default{
     "imagen_AA_proceso_1": imagen_AA_proceso_1,
     "imagen_AA_proceso_2": imagen_AA_proceso_2,
     "imagen_AA_proceso_3": imagen_AA_proceso_3,
+
+    "imagen_LL_proceso_1" : imagen_LL_proceso_1,
+    "imagen_LL_proceso_2" : imagen_LL_proceso_2,
+    "imagen_LL_proceso_3" : imagen_LL_proceso_3,
 
     "logo_IHC_normal": logo_IHC_normal,
     "logo_LaunchLab_blanco": logo_LaunchLab_blanco,
