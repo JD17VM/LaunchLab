9b1ce875d258c03b64dbe32292e415f29363707f
JD17VM <jdiegodiego4@gmail.com>
2024-12-16 14:44:31 -0500

    fix

--- projects/IHC/src/proyectos.jsx
+++ projects/IHC/src/proyectos.jsx
@@ -1,31 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Navegador} from './widgets/Navegador'
-import { Footer } from './widgets/Footer'
 
 import './assets/styles/estilos_proyectos.css'
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <Navegador></Navegador>
+export function Proyectos() {
+    return (
+        <>
+            <div className="contenido">
+                <div className="seccion">
+                    <div className="contenido_seccion">
+                        <a href="../index.html" className="cont_archivo">
+                            <img src="../static/imgs/Imagen_Archivos.PNG" alt="" />
+                            <p>Launch Lab VR</p>
+                        </a>
 
-    <div class="contenido">
-        <div class="seccion">
-            <div class="contenido_seccion">
-                <a href="../index.html" class="cont_archivo">
-                    <img src="../static/imgs/Imagen_Archivos.PNG" alt=""/>
-                    <p>Launch Lab VR</p>
-                </a>
-
-                <a href="../index2.html" class="cont_archivo">
-                    <img src="../static/imgs/Imagen_Archivos.PNG" alt=""/>
-                    <p>Arrow Ascend</p>
-                </a>
+                        <a href="../index2.html" className="cont_archivo">
+                            <img src="../static/imgs/Imagen_Archivos.PNG" alt="" />
+                            <p>Arrow Ascend</p>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-
-    <Footer></Footer>
-  </>
-)
+        </>
+    )
+}
