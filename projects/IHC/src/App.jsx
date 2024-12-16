9beee0a4410519e976202c9a8ff232e89aa267fa
JD17VM <jdiegodiego4@gmail.com>
2024-12-16 15:45:42 -0500

    .

--- projects/IHC/src/App.jsx
+++ projects/IHC/src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Navegador} from './widgets/Navegador'
 import { Footer } from './widgets/Footer'
 
+import { Inicio } from './index'
+import { Nosotros } from './nosotros'
+//import { Documentos } from './documentos'
+//import { Proceso } from './proceso'
+import { Proyectos } from './proyectos'
 
 
-const Home = () => <h1>Home</h1>;
 const Documentos = () => <h1>Documentos</h1>;
 const Proceso = () => <h1>Proceso</h1>;
-const Nosotros = () => <h1>Nosotros</h1>;
-const Proyectos = () => <h1>Proyectos</h1>;
+
+
 
 const App = () => {
   return (
@@ -17,7 +21,7 @@ const App = () => {
 
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Inicio />} />
           <Route path="/documentos" element={<Documentos />} />
           <Route path="/proceso" element={<Proceso />} />
           <Route path="/nosotros" element={<Nosotros />} />
--- projects/IHC/src/nosotros.jsx
+++ projects/IHC/src/nosotros.jsx
@@ -1,50 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Navegador} from './widgets/Navegador'
-import { Footer } from './widgets/Footer'
 import { Cabecera } from './widgets/Cabecera'
 
 import './assets/styles/estilos_nosotros.css'
 
-import Nosotros from './assets/imgs/nosotros.png';
 
 import imageHelper from './utils/imageHelper'
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <Navegador></Navegador>
+export function Nosotros() {
+  return (
+    <>
 
-    <Cabecera background_image="background_nosotros.webp">Nosotros</Cabecera>
+      <Cabecera background_image="background_nosotros.webp">Nosotros</Cabecera>
 
-    <div className="contenedor">
+      <div className="contenedor">
 
-      <p className="texto">
-        Como estudiantes de la carrera de Ciencias de la Computación en la Universidad Nacional de San Agustín, nos encontramos profundamente interesados en el diseño y desarrollo de aplicaciones basadas en realidad virtual (VR). Este proyecto representa una oportunidad emocionante para poner en práctica nuestros conocimientos, superar desafíos tecnológicos y potenciar nuestras habilidades técnicas y creativas.
-      </p>
+        <p className="texto">
+          Como estudiantes de la carrera de Ciencias de la Computación en la Universidad Nacional de San Agustín, nos encontramos profundamente interesados en el diseño y desarrollo de aplicaciones basadas en realidad virtual (VR). Este proyecto representa una oportunidad emocionante para poner en práctica nuestros conocimientos, superar desafíos tecnológicos y potenciar nuestras habilidades técnicas y creativas.
+        </p>
 
-      <div className="cont_integrantes">
+        <div className="cont_integrantes">
 
-        <div className="integrante">
-          <img src={imageHelper.foto_JuanDiego} alt="" />
-          <div>
-            <p>Juan Diego Valdivia Mendoza</p>
-            <p>Full Stack Developer</p>
+          <div className="integrante">
+            <img src={imageHelper.foto_JuanDiego} alt="" />
+            <div>
+              <p>Juan Diego Valdivia Mendoza</p>
+              <p>Full Stack Developer</p>
+            </div>
           </div>
-        </div>
 
-        <div className="integrante">
-          <img src={imageHelper.foto_Alex} alt="" />
-          <div>
-            <p>Alex Enrique Mamani Quispe</p>
-            <p>Full Stack Developer</p>
+          <div className="integrante">
+            <img src={imageHelper.foto_Alex} alt="" />
+            <div>
+              <p>Alex Enrique Mamani Quispe</p>
+              <p>Full Stack Developer</p>
+            </div>
           </div>
+
         </div>
 
       </div>
 
-    </div>
+    </>
+  )
 
-    <Footer></Footer>
-  </>
-)
+}
