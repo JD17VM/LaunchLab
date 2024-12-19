111afe5e66065dfd79a2fba144b04f32e753fbed
JD17VM <jdiegodiego4@gmail.com>
2024-12-19 03:14:23 -0500

    m

--- projects/IHC/src/proyectos.jsx
+++ projects/IHC/src/proyectos.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Cabecera } from './widgets/Cabecera'
 
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 import imageHelper from './utils/imageHelper'
 
 import styles from './assets/styles/estilos_proyectos.module.css'
@@ -13,19 +15,19 @@ export function Proyectos() {
             <div className={styles.contenido}>
                 <div className={styles.seccion}>
                     <div className={styles.contenido_seccion}>
-                        <a href="../" className={styles.cont_archivo} >
+                        <Link to="/" className={styles.cont_archivo} >
                             <div style={{ backgroundImage: `url(${imageHelper.background_image_AA_proyecto})` }}>
                                 <img src={imageHelper.logo_ArrowAscend_fondoNegro} alt=""/>
                             </div>
                             <p>Launch Lab VR</p>
-                        </a>
+                        </Link>
 
-                        <a href="../index2" className={styles.cont_archivo} >
+                        <Link to="/index2" className={styles.cont_archivo} >
                             <div style={{ backgroundImage: `url(${imageHelper.background_image_LL_proyecto})` }}>
                                 <img src={imageHelper.logo_LaunchLab_blanco} alt=""/>
                             </div>
                             <p>Launch Lab VR</p>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
